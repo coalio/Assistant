f367d0affa603853933eac0217349fe6e1175cf6
coalio <dibashtori@gmail.com>
2020-10-04 11:34:15 -0600

    Fix capitalization in filenames

---
+++ assistant/lib/classes/sheet/public/actions/index.lua
@@ -0,0 +1,8 @@
+-- sheet.lua
+-- Sheet data action functions
+
+return {
+  head   = require(BASE(..., 'head')),
+  tail   = require(BASE(..., 'tail')),
+  select = require(BASE(..., 'select')),
+} 
--- assistant/lib/classes/sheet/public/max.lua
+++ assistant/lib/classes/sheet/public/max.lua
@@ -6,6 +6,5 @@ return function(data, n)
   if not n then return math.max(unpack(data)) end
   local t = (function() local a={} for k,v in next, data, nil do a[k]=v end return a end)()
   table.sort(t, function(at, of) return at > of end)
-  local min = growTable(t, n)
-  return min
+  return growTable(t, n)
 end
